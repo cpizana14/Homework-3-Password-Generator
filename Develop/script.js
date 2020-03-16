73fba72088922abf370188f3646482fe3cdfd119
Christian Pizana <c.pizana14@gmail.com>
2020-03-16 00:03:11 -0600

    hw upload

---
+++ Develop/script.js
@@ -0,0 +1,52 @@
+var char = 'abcdefghijklmnopqrstuvwxyz'
+var uchar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var num = '0123456789'
+var schar = '!"#$%&()*+,-./<>=?@_`~|^'
+var password = ""
+
+
+var howMany = prompt("how many charcters would you like? Must be between 8-128")
+
+  if (howMany >= 8 && howMany <= 128) {
+
+    var upper = confirm("would you like uppercase letters");
+    if (upper === true) {
+        password += uchar;
+    }
+
+    var lower = confirm("would you like lowercase letters");
+    if (lower === true) {
+        password += char;
+    }
+
+    var numbers = confirm("would you like numbers");
+    if (numbers === true) {
+        password += num;
+    }
+
+    var special = confirm("would you like special characters");
+    if (special === true) {
+        password += schar;
+    }
+}
+
+else{confirm("must be bewtween 8 and 128 characters")
+document.location.reload()}
+    
+console.log(password)
+function generate() {
+ var answer = "";
+ for (i = 1; i <= howMany; i++) {
+    answer += password.charAt(Math.floor(Math.random() * (password.length)));
+    }
+    document.getElementById("text1").value = answer;
+   
+}
+console.log(generate());
+function copyPassword(){
+ 
+   document.getElementById("text1").select();
+   document.execCommand("Copy");
+   alert("password has been copied");
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Homework-3-Password-Generator
+Password Generator
+
+This page will generate a password out of different numbers, letter, and special characters.
+
+
+The page will ask a question on how many characters you'd like, it must be between 8 and 128 characters long, and then it will ask if you'd like lowercase, uppercase, numbers or special charcters. 
+
+
